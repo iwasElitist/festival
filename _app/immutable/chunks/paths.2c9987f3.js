955b3f03255cb017d954f6073d0b399c39aef63d
iwasElitist <ritwiknair71@gmail.com>
2023-05-28 16:10:05 +0530

    Updates

--- _app/immutable/chunks/paths.2c9987f3.js
+++
@@ -1,1 +0,0 @@
-var l,s;const a=(null==(l=globalThis.__sveltekit_1lkqw0k)?void 0:l.base)??"/festival",e=(null==(s=globalThis.__sveltekit_1lkqw0k)?void 0:s.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/chunks/paths.ebbc592c.js
@@ -0,0 +1,1 @@
+var s,l;const a=(null==(s=globalThis.__sveltekit_cgs6ph)?void 0:s.base)??"/festival",e=(null==(l=globalThis.__sveltekit_cgs6ph)?void 0:l.assets)??a;export{e as a,a as b};
